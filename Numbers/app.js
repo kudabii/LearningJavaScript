d1a1809893f31f6f1b66042a4fdde0e8b1efb381
Khalid Okorejior <kudabik@gmail.com>
2026-08-28 22:09:31 +0100

    Learning about numbers  and the math operator

---
+++ Numbers/app.js
@@ -0,0 +1,24 @@
+const num1 = 100;
+const num2 = 250;
+
+//Simple math operations with numbers
+
+val = num1 + num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 - num2;
+val = num2 % num1; // it gives the remainder after division .... 100/250 = 0 with 100 leftover
+
+// Math Object
+val = Math.PI; // pi - 3.14...
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4); // rounds up to the nearest whole... 3
+val = Math.floor(2.4); // rounds down to the nearest whole..2
+val = Math.sqrt(64); //squar-root
+val = Math.abs(-3); //returns the absolute value regardless if its + or -
+val = Math.pow(8,2); // 8 raised to power  2 = 64
+val = Math.min(2, 23,232, 4, 423, 21); // returns the minimum
+val = Math.max(2, 23,232, 4, 423, 21); //returns the highest value
+val = Math.min(2, 23,232, 4, 423, 21);
+console.log(val);
