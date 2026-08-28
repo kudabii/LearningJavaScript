7644bd9e49c090421b8addab601512a9fcd357e5
Khalid Okorejior <kudabik@gmail.com>
2026-08-28 15:38:06 +0100

    Learnt Variables - var, let & const

---
+++ Variables/app.js
@@ -0,0 +1,45 @@
+// var, let, const
+
+var name = "Khalid";
+console.log(name);
+
+name = 'Imoukhuede';
+console.log(name);
+
+//var and let could be reassigned, while const can not
+//the only characters that could be in a variable ; letters, numbers, _, $
+//variables can't start with numbers, 
+
+
+var 1name =  'khalid';
+console.log(1name);
+
+//result would show 'error'
+
+
+//multi word vars
+var firstName // camelcase
+var first_Name // underscore
+var FirstName // pascal case
+
+
+//const stands for 'Constant' - meaning it can't change [can't be reassigned] and also it must be assigned or declared
+
+const lastName;
+lastName = "okorejior";
+console.log(lastName);
+
+
+//const value can be changed but not reassigned...
+
+const person = {
+  name: "Khalid",
+  age: 99
+}
+
+person.name = "jacob";
+person.age = 32;
+
+console.log(name);
+
+//name would be changed to "jacob"
