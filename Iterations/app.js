c862fd0dd39bdcb1fbb303fa7e7d715b3b1713a2
Khalid Okorejior <kudabik@gmail.com>
2026-08-31 12:25:46 +0100

    Organize JS fundamentals into single folder

---
+++ Iterations/app.js
@@ -0,0 +1,9 @@
+//Loop is an instruction that repeats until a specific condition is reached ; FOR loops WHILE loop and DO WHILE loops
+// var or let is used inside a for loop cause its going to keep changing
+
+//the first is the declaration of the variable , the second is the condition , the last is the increment of i
+for(let i = 0; i <= 10; i++ ){
+  console.log(i);
+}
+
+//result is the counting of 0-10
